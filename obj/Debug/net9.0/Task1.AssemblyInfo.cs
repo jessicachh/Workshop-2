b2c3560716631d183b7635ff00f0fdfd393b40af
Jesika Raut <jsscraut@gmail.com>
2025-11-12 19:45:21 +0545

    Task-1 Code

--- obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa3ed439e8dbd8fb1cd2529ea2e05d6ffa42067")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
