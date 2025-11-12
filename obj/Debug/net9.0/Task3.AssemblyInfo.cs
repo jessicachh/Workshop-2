b6443d438d8805eb17faa5f8330286b8477290a4
Jesika Raut <jsscraut@gmail.com>
2025-11-12 19:56:57 +0545

    Task-3 Code

--- obj/Debug/net9.0/Task3.AssemblyInfo.cs
+++ obj/Debug/net9.0/Task3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc128a05185abe2f8e364bb4ad7ff2016fe71bbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
